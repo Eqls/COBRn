0170c8fffe6df023e00f8efb104335ec165ab9f6
benonymus <bence1204@gmail.com>
2018-11-01 23:08:05 +0700

    requested cosmetic changes and bug fixes

--- src/components/challengecard/TeamMembersBar.js
+++ src/components/challengecard/TeamMembersBar.js
@@ -1,14 +1,8 @@
 import React from "react";
-import {
-  View,
-  Text,
-  StyleSheet,
-  Image,
-  TouchableOpacity
-} from "react-native";
-import { DefaultAvatar } from '../../assets/images'
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
+import { DefaultAvatar } from "../../assets/images";
 import config from "../../config/config";
-import { Actions } from 'react-native-router-flux'
+import { Actions } from "react-native-router-flux";
 
 const styles = StyleSheet.create({
   container: {
@@ -19,34 +13,41 @@ const styles = StyleSheet.create({
     paddingRight: 20,
     paddingLeft: 20,
     paddingTop: 5,
-    paddingBottom: 5,
+    paddingBottom: 5
   },
   avatar_img: {
     height: 50,
     width: 50,
     borderRadius: 100,
     borderWidth: 3,
-    borderColor: 'white'
+    borderColor: "white"
   },
   avatar: {
     padding: 3
   }
 });
 
-isDone = done => done && { borderColor: 'lightgreen' }
+isDone = done => done && { borderColor: "lightgreen" };
 
 const TeamMembersBar = ({ users }) => (
   <View style={styles.container}>
-    {users.map(user =>
-      <TouchableOpacity onPress={() => Actions.myprofile({ uid: user.id })} style={styles.avatar}>
+    {console.log(users)}
+    {users.map(user => (
+      <TouchableOpacity
+        onPress={() => Actions.myprofile({ uid: user.id })}
+        style={styles.avatar}
+      >
         <Image
           style={[styles.avatar_img, isDone(user.done)]}
-          source={user.avatar ? { uri: config.PHOTO_URL + user.avatar } : DefaultAvatar}
+          source={
+            user.avatar
+              ? { uri: config.PHOTO_URL + user.avatar }
+              : DefaultAvatar
+          }
         />
       </TouchableOpacity>
-    )
-    }
-  </View >
-)
+    ))}
+  </View>
+);
 
 export default TeamMembersBar;
--- src/screens/Login.js
+++ src/screens/Login.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { Actions } from 'react-native-router-flux'
+import React from "react";
+import { Actions } from "react-native-router-flux";
 import {
   View,
   Text,
@@ -11,30 +11,30 @@ import {
   Dimensions,
   TouchableOpacity,
   ImageBackground
-} from 'react-native'
-import { connect } from 'react-redux'
-import { auth } from '../actions'
-import styleConsts from '../constants/styles'
-import { CheckinIcon, LoginBg, Logo } from '../assets/images'
-import ProgressBar from '../components/challengecard/ProgressBar'
+} from "react-native";
+import { connect } from "react-redux";
+import { auth } from "../actions";
+import styleConsts from "../constants/styles";
+import { CheckinIcon, LoginBg, Logo } from "../assets/images";
+import ProgressBar from "../components/challengecard/ProgressBar";
 
 class Login extends React.Component {
   state = {
     user: {
-      name: '',
-      password: ''
+      name: "",
+      password: ""
     }
-  }
+  };
 
   componentDidMount() {
-    if (this.props.isLogged) Actions.home()
+    if (this.props.isLogged) Actions.home();
   }
 
   sendLoginRequest = () => {
-    const { dispatch } = this.props
-    const { user } = this.state
-    dispatch(auth.login(user))
-  }
+    const { dispatch } = this.props;
+    const { user } = this.state;
+    dispatch(auth.login(user));
+  };
 
   handleChange = data => {
     this.setState({
@@ -43,14 +43,14 @@ class Login extends React.Component {
         ...this.state.user,
         ...data
       }
-    })
-  }
+    });
+  };
 
   render() {
-    const { user } = this.state
-    const { isLogged, error } = this.props
+    const { user } = this.state;
+    const { isLogged, error } = this.props;
     if (isLogged) {
-      Actions.home()
+      Actions.home();
     }
     return (
       <View style={styles.container}>
@@ -58,7 +58,7 @@ class Login extends React.Component {
           <Text style={styles.error}>
             {error.error
               ? error.error
-              : 'Er is iets fout gegaan! Probeer het later opnieuw.'}
+              : "Er is iets fout gegaan! Probeer het later opnieuw."}
           </Text>
         )}
         <View style={styles.logo_wrapper}>
@@ -82,7 +82,7 @@ class Login extends React.Component {
             placeholder="Typ je gebruikersnaam"
             autoCapitalize="none"
             underlineColorAndroid="transparent"
-            textAlign={'center'}
+            textAlign={"center"}
           />
           <TextInput
             style={styles.formField}
@@ -93,14 +93,14 @@ class Login extends React.Component {
             placeholder="Voer uw wachtwoord in"
             autoCapitalize="none"
             underlineColorAndroid="transparent"
-            textAlign={'center'}
+            textAlign={"center"}
             secureTextEntry
           />
           <TouchableOpacity
             onPress={this.sendLoginRequest}
             style={styles.button}
           >
-            <Text style={{ fontSize: 16, color: 'white', fontWeight: 'bold' }}>
+            <Text style={{ fontSize: 16, color: "white", fontWeight: "bold" }}>
               Login
             </Text>
           </TouchableOpacity>
@@ -109,83 +109,84 @@ class Login extends React.Component {
           </View>
         </View>
       </View>
-    )
+    );
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    backgroundColor: 'white'
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "flex-end",
+    backgroundColor: "white"
   },
   formField: {
     height: 50,
-    backgroundColor: '#f2f2f2',
+    backgroundColor: "#f2f2f2",
     borderRadius: 50,
-    width: '100%',
+    width: "100%",
     marginBottom: 15,
     zIndex: 1,
-    color: '#010763'
+    color: "#010763"
   },
   error: {
-    color: 'red',
-    backgroundColor: '#f2f2f2',
+    color: "red",
+    backgroundColor: "#f2f2f2",
     padding: 5,
     paddingLeft: 20,
     paddingRight: 20,
-    borderRadius: 5
+    borderRadius: 5,
+    zIndex: 999
   },
   button: {
-    display: 'flex',
-    alignItems: 'center',
+    display: "flex",
+    alignItems: "center",
     borderRadius: 50,
     padding: 12,
     zIndex: 2,
     backgroundColor: styleConsts.login_font_color
   },
   form: {
-    width: '100%',
+    width: "100%",
     padding: 10,
     marginBottom: 20,
     zIndex: 2
   },
   checkin: {
-    position: 'absolute',
+    position: "absolute",
     right: 40,
     bottom: -12,
     zIndex: 999
   },
   checkin_icon: {
     aspectRatio: 0.25,
-    resizeMode: 'contain'
+    resizeMode: "contain"
   },
   background_wrapper: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 40
   },
   background_img: {
     aspectRatio: 0.5,
-    resizeMode: 'contain'
+    resizeMode: "contain"
   },
   logo_wrapper: {
-    position: 'absolute',
+    position: "absolute",
     bottom: 420,
     zIndex: 2
   },
   logo_img: {
     aspectRatio: 2,
-    resizeMode: 'contain'
+    resizeMode: "contain"
   }
-})
+});
 
 const mapStateToProps = state => ({
   isFetching: state.auth.isFetching,
   error: state.auth.error,
   isLogged: state.auth.isLogged
-})
+});
 
-export default connect(mapStateToProps)(Login)
+export default connect(mapStateToProps)(Login);
